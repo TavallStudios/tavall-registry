5349f8a726a813a8c83050784693197a91332eb6
TJ <tjxjnoobie@gmail.com>
2026-09-25 16:37:30 +0000

    ci: capture immutable library artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.tasks.Copy
 import java.util.zip.ZipFile
 
 plugins {
@@ -82,4 +83,13 @@ publishing {
             }
         }
     }
+}
+
+
+tasks.register<Copy>("tavallCiArtifact") {
+    val binaryJar = tasks.named<Jar>("jar")
+    dependsOn(binaryJar)
+    from(binaryJar.flatMap { it.archiveFile })
+    into(layout.buildDirectory.dir("tavall-ci-artifacts"))
+    rename { "tavall-registry.jar" }
 }
